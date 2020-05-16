5decbeab7639de2018bc595577e530e2312fc2b3
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-15 23:58:35 -0700

    moved files around

--- src/remote/auther-service.ts
+++
@@ -1,6 +0,0 @@
-import {projectOneClient} from './projectOne-client';
-
-export async function authenticate(username: string, password: string){
-	let response = await projectOneClient.post('./auth',{username, password});
-	return await response.data;
-}
