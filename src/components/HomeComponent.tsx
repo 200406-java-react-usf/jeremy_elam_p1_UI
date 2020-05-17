189769b0afa5b67294ed0eb8e7aaf4d7b08cbc77
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-16 21:09:06 -0700

    added HomeComponent to UI

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,1 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+import {Button} from '@material-ui/core';
+import {getUsers} from '../remote/user-service';
+import {allReimb} from '../remote/reimb-service'
+import {logout} from '../remote/auth-service';
+
+interface IHomeProps{
+	username: string;
+}
+
+const HomeComponent = (props: IHomeProps) =>{
+	return (
+		!props.username ?
+		<Redirect to = "/login" /> :
+		<>	
+			<br></br>
+			<br/>
+			<h1>Welcome, {props.username}! </h1>
+			
+
+			<br/>
+			<Button onClick = {getUsers} variant = "contained" color = "primary" size = "medium">Get All Users</Button>
+			<br/>
+			<Button onClick = {logout} variant = "contained" color = "primary" size = "medium"> Logout</Button>
+			<br/>
+			<Button onClick = {allReimb} variant = "contained" color = "secondary" size = "small"> Get All Reimb Stuff</Button>
+		</>
+	)
+}
+
+export default HomeComponent;
