d2b34b9b8a62ab62bdcc489bb393d0461b3ddbef
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-16 21:20:40 -0700

    added get all reimb connection

--- src/remote/reimb-service.ts
+++ src/remote/reimb-service.ts
@@ -4,3 +4,9 @@ export async function newReimb(amount: number, description: string, author_id: n
 	let response =  await projectOneClient.post('/reimb',{amount, description, author_id, reimb_type})
 	return await response.data;
 }
+
+export async function allReimb(){
+	let response = await projectOneClient.get('/reimb');
+	console.log(response.data);
+	return await response.data;
+}
