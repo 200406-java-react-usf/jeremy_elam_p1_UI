545e90947aac6d265608f1258034c591bf6b93f9
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-18 16:02:46 -0700

    added new axios request to api

--- src/remote/reimb-service.ts
+++ src/remote/reimb-service.ts
@@ -5,8 +5,32 @@ export async function newReimb(amount: number, description: string, author_id: n
 	return await response.data;
 }
 
+export async function updateReimb(reimb_id: number, resolver_id: number, reimb_status: string){
+	let response = await projectOneClient.put('/reimb',{reimb_id, resolver_id, reimb_status});
+	console.log(response.data);
+	return await response.data;
+}
+
 export async function allReimb(){
 	let response = await projectOneClient.get('/reimb');
 	console.log(response.data);
 	return await response.data;
+}
+
+export async function pendingReimb(){
+	let response = await projectOneClient.get('/reimb/status/pending');
+	console.log(response.data);
+	return await response.data
+}
+
+export async function approvedReimb(){
+	let response = await projectOneClient.get('/reimb/status/approved');
+	console.log(response.data);
+	return await response.data
+}
+
+export async function deniedReimb(){
+	let response = await projectOneClient.get('/reimb/status/denied');
+	console.log(response.data);
+	return await response.data
 }
