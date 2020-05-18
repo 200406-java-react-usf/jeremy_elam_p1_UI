84ac3b912300b9448c6a63ffeb016d95521fcbbd
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-17 21:28:21 -0700

    added minor change to homepage route

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import { AppBar, Toolbar, Typography, createMuiTheme, ThemeProvider } from '@mat
 import {Users} from './models/users';
 import {Reimbursements} from './models/reimbs';
 
-
 import LoginComponent from './components/LoginComponent';
 import RegisterComponent from './components/RegisterComponent';
 import ReimbComponent from './components/NewReimbsComponent';
 import HomeComponent from './components/HomeComponent';
 import NavbarComponent from './components/NavbarComponent';
 import UserComponent from './components/AllUserComponent'
-
-
 
 import logo from './logo.svg';
 import './App.css';
@@ -38,7 +35,7 @@ function App(){
         <AppBar color = "primary" position = "static">
           <Toolbar>
             <Typography>
-              <NavbarComponent authUser = {authUser}/>
+              <NavbarComponent authUser = {authUser} setAuthUser = {setAuthUser}/>
             </Typography>
           </Toolbar>
         </AppBar>
@@ -46,13 +43,13 @@ function App(){
         <br/>
         
         <Switch>
-          <Route path="/home" render = {() => <HomeComponent username = {authUser?.username}/>}/>
+          <Route path="/home" render = {() => <HomeComponent authUser = {authUser}/>}/>
           <Route path="/login" render={() => <LoginComponent authUser={authUser} setAuthUser={setAuthUser} />} />
           <Route path = "/register" render = {() =><RegisterComponent newUser = {newUser} setNewUser = {setNewUser}/>}/>
           <Route path = "/users" render = {() => <UserComponent authUser = {authUser}/>}/>
           <Route path = "/user/update" render = {() =><UpdateUserComponent updateUser = {updateUser} setUpdateUser = {setUpdateUser}/>}/>
           <Route path = "/reimb/all" render = {() => <AllReimbComponent authUser = {authUser} allReimb = {newReimb} />}/>
-          <Route path = "/reimb" render = {()=><ReimbComponent username = {authUser?.username} newReimb = {newReimb} setNewReimb = {setNewReimb}/>} />
+          <Route path = "/reimb" render = {()=><ReimbComponent username = {authUser?.username} newReimb = {newReimb} setNewReimb = {setNewReimb}/>} /> 
         </Switch>
       </Router>
     </>
