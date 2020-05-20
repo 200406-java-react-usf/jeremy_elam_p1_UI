183835953d2f85c0db5d81caa4d75574f834dc6d
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-20 00:15:27 -0700

    changed submitted and resolved from dates to string

--- src/models/reimbs.ts
+++ src/models/reimbs.ts
@@ -1,15 +1,15 @@
 export class Reimbursements{
 	reimb_id: number;
 	amount: number;
-	submitted: Date;
-	resolved: Date;
+	submitted: string;
+	resolved: string;
 	description: string;
 	author_id: number;
 	resolver_id: number;
 	reimb_status: string;
 	reimb_type: string;
 
-	constructor(reimb_id: number, amount: number, submitted: Date, resolved: Date, description: string, author: number, resolver: number, status_id: string, type_id: string){
+	constructor(reimb_id: number, amount: number, submitted: string, resolved: string, description: string, author: number, resolver: number, status_id: string, type_id: string){
 		this.reimb_id = reimb_id;
 		this.amount = amount;
 		this.submitted = submitted;
