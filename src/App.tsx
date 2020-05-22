5b768fa9f8bc1610e616bc947639edc1c88d3d05
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-21 17:54:49 -0700

    update app.tsx with info needed

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,12 @@ import {Reimbursements} from './models/reimbs';
 import LoginComponent from './components/LoginComponent';
 import RegisterComponent from './components/RegisterComponent';
 import ReimbComponent from './components/NewReimbsComponent';
-import PendingReimbComponent from './components/PendingReimbComponent'
-import ApprovedReimbComponent from './components/ApprovedReimbComponent'
 import HomeComponent from './components/HomeComponent';
 import NavbarComponent from './components/NavbarComponent';
 import UserComponent from './components/AllUserComponent'
 import UpdateUserComponent from './components/UpdateUserComponent';
 import AllReimbComponent from './components/AllReimbComponent';
-import DeniedReimbComponent from './components/DeniedReimbComponent';
 import ReimbIdComponent from './components/ReimbIdComponent';
-import DeleteComponent from './components/DeleteTargetUser';
 import ReimbDetailsComponent from './components/DetailReimbComponent';
 import UpdateRimbComponent from './components/UpdateReimbComponent';
 import logo from './logo.svg';
@@ -56,20 +52,15 @@ function App(){
         <Switch>
           <Route path="/home" render = {() => <HomeComponent authUser = {authUser}/>}/>
           <Route path="/login" render={() => <LoginComponent authUser={authUser} setAuthUser={setAuthUser} />} />
-          <Route path = "/register" render = {() =><RegisterComponent newUser = {newUser} setNewUser = {setNewUser}/>}/>
+          <Route path = "/register" render = {() =><RegisterComponent authUser = {authUser} newUser = {newUser} setNewUser = {setNewUser}/>}/>
           <Route exact path = "/users" render = {() => <UserComponent authUser = {authUser} setThisUser = {setThisUser}/>}/>
-          <Route exact path = "/users/delete" render = {() => <DeleteComponent authUser = {authUser} setDeleteUser = {setDeleteUser}/>}/>
-          <Route path = "/user/update" render = {() =><UpdateUserComponent updateUser = {updateUser} authUser = {authUser}/>}/>
+          <Route path = "/user/update" render = {() =><UpdateUserComponent updateUser = {updateUser} authUser = {authUser} setUpdateUser ={setUpdateUser}/>}/>
           <Route path = "/reimb/all" render = {() => <AllReimbComponent authUser = {authUser} setThisReimb = {setThisReimb}  />}/>
           <Route path = "/reimb/id" render = {() => <ReimbIdComponent authUser = {authUser} setThisReimb = {setThisReimb}  />}/>
-          <Route path = "/reimb/status/pending" render = {() => <PendingReimbComponent authUser = {authUser}  setThisReimb = {setThisReimb}/>}/>
-          <Route path = "/reimb/status/approved" render = {() => <ApprovedReimbComponent authUser = {authUser}  setThisReimb = {setThisReimb} />}/>
-          <Route path = "/reimb/status/denied" render = {() => <DeniedReimbComponent authUser = {authUser} setThisReimb = {setThisReimb}   />}/>
           <Route path = "/reimb/update" render = {() =><UpdateRimbComponent authUser = {authUser} setNewReimb = {setNewReimb}/>}/>
           <Route path = "/reimb" render = {()=><ReimbComponent authUser = {authUser} newReimb = {newReimb} setNewReimb = {setNewReimb}/>} /> 
           <Route path = {`/details-${thisReimb.reimb_id}`} render = {() => <ReimbDetailsComponent authUser = {authUser} thisReimb = {thisReimb}/>}/>
       </Switch>
-
     </Router>
     </>
   );
